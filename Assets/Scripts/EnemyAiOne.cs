e43c2e5a2f609b917204ea290387c09491f75349
PeteyPablo7 <petergartzke@Peters-MacBook-Pro-2.local>
2016-09-30 13:21:59 -0500

    Cleaned Up
    I clean up the scripts.

--- Assets/Scripts/EnemyAiOne.cs
+++ Assets/Scripts/EnemyAiOne.cs
@@ -3,26 +3,13 @@ using System.Collections;
 
 public class EnemyAiOne : MonoBehaviour 
 {
-	//public float moveSpeed;			//The enemy's speed at which it can move at
-	//public float damage = 5;				//The damage that the enemy will deal when attacking the target
-	//public float chaseDist;
-	//public float attackDist;		//The distance at which the enemy will stop moving towards the target to attack
-
-	//public float dist;				//The distance at which the enemy is from the target
-	//public float distance;
+	
 	public int scoreToGive;			//The score that will be added once the enemy is killed
-
-	//public GameObject target;		//The GameObject of which the enemy will move towards and attack
+	public int Damage;
+		
 	public float timer = 1.0f;
 
-
-	//public bool walking = true;
-	//public bool punch = true;
-	//public float timer = 1.0f;
-	public Transform goal;
-	//public float stoppingDistance;
-
-	public int Damage;
+	public Transform goal;   //The GameObject of which the enemy will move towards and attack
 
 	void Awake ()
 	{
@@ -37,23 +24,6 @@ public class EnemyAiOne : MonoBehaviour
 		agent.destination = goal.position;
 
 	}
-
-	// Update is called once per frame
-	//void Update () 
-	//{
-
-
-
-	//if (goal.position < 6) {
-
-	//}
-
-
-	//}
-
-
-
-
 	public void OnTriggerStay(Collider other)
 	{
 		if(other.gameObject.tag == "Tower")
--- Assets/Scripts/TowerHealth.cs
+++ Assets/Scripts/TowerHealth.cs
@@ -1,52 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TowerHealth : MonoBehaviour {
+public class TowerHealth : MonoBehaviour 
+{
 
-	//public int playerAmount;
+
 	public float maxHealth = 100f;
 	public float curHealth = 0f;
-	//public GameObject healthBar;
+
 	public bool alive = true;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		alive = true;
 		curHealth = maxHealth;
 	}
-
-	// Update is called once per frame
-	void Update () {
-
-
-
-	}
-	/*public void TakeDamage (float amount) 	{ 		if (!alive)  		{ 			return;  		} 		if (curHealth <= 0)  		{ 			curHealth = 0; 			alive = false; 			//gameObject.SetActive (false); 		}  		curHealth -= amount; 		SetHealthBar (); 	}  */
-
-
 	public void EnemyHit(int amount)
 	{    
-
-		curHealth -=  amount;
-		float myHealth = curHealth / maxHealth;
-
-
-		if (curHealth <= 0) {
+	curHealth -=  amount;
+	float myHealth = curHealth / maxHealth;
+		if (curHealth <= 0) 
+		{
 
 			Die ();
 		}
 	}
 	void Die()
 	{   
-
-
-		Destroy (this.gameObject);
-
+	Destroy (this.gameObject);
 	}
-	/*public void PlayerHit(int playerAmount) 	{ 		curHealth -= playerAmount; 		float myHealth = curHealth / maxHealth;   		if (curHealth <= 0) {  			Destroy (); 		} 	} 	*/void Destroy()
+	void Destroy()
 	{   
-
-
-		Destroy (this.gameObject);
+	Destroy (this.gameObject);
 	}
 }
 
