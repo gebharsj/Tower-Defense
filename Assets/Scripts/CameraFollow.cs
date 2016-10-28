dbc88c5bd009d58b91115c3ddcde384ac426a433
gebharsj <gebharsj@gmatc.matc.edu>
2016-10-28 15:02:30 -0500

    added cannon, updated the tower to have four sections

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject cameraAnchor;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        if (cameraAnchor != null)
+        {            
+            yield return new WaitUntil(LerpTo);
+            StartCoroutine(Start());
+        }
+	}
+
+    bool LerpTo()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraAnchor.transform.position + new Vector3(0, 2.85f, 0), Time.deltaTime * 3);
+
+        if (Vector3.Distance(transform.position, cameraAnchor.transform.position + new Vector3(0, 2.85f, 0)) <= 0.5)
+            return true;
+        else
+            return false;
+    }
+}
