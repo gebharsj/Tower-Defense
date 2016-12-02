33d488c60b3adcddc8c0ce0ea035a31b57d12331
PeteyPablo7 <pgartzke@gmail.com>
2016-12-02 12:39:49 -0600

    Main Menu buttons
    added main menu buttons

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -4,8 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class MenuButton : MonoBehaviour {
 
+   
 	public void Play(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+    public void Quit() //When you press Quit, this quits Unity.
+    {
+       
+        Application.Quit();
+    }
+
+
 }
