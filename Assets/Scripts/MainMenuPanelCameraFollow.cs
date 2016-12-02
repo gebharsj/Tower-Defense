e5ab2bd8ad134d18947c5fc652ff11b6efbd20aa
PeteyPablo7 <pgartzke@gmail.com>
2016-12-02 16:05:01 -0600

    Updated the main menu
    updated main menu buttons and the panels for instructions and credits

---
+++ Assets/Scripts/MainMenuPanelCameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuPanelCameraFollow : MonoBehaviour {
+   
+    
+    public GameObject mainMenuPanel;
+    public Transform target;
+    // Use this for initialization
+    void Start () {
+       
+       mainMenuPanel.transform.LookAt(target);
+        mainMenuPanel.transform.Rotate(0, 180, 0);
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
